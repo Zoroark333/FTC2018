91b5f0aa766b811f1e6106f1868835d83703733d
AidanSpiller <awspiller04@gmail.com>
2017-12-02 11:03:33 -0700

    created init

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltraSonicBetaAutonomous.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltraSonicBetaAutonomous.java
@@ -1,31 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by njmin on 2017-12-02.
  */
 
+@Autonomous(name="UltraSonicTest", group="Autonomous")
 public class UltraSonicBetaAutonomous extends LinearOpMode {
-    
 
+    Hardware robot = new Hardware();
 
-
-
-
+    UltrasonicSensor ultrasonicSensor = robot.hwMap.ultrasonicSensor.get("frontUltraSonic");
 
     @Override
     public void runOpMode() throws InterruptedException {
         //init code
 
 
+    ultrasonicSensor.getUltrasonicLevel();
 
-
+        telemetry.addData("ready!!!!!!", "go");
         waitForStart();
-        // wiat for play button pressed
+        // wait for play button pressed
 
         while (opModeIsActive()){
 
+            telemetry.addData("Range", String.valueOf(ultrasonicSensor.getUltrasonicLevel()));
 
         }
     }
