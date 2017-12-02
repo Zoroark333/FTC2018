5b187f7f77db8e0a4ba07d9b50304b1a2233d6ec
AidanSpiller <awspiller04@gmail.com>
2017-12-02 10:30:24 -0700

    Created class and init

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltraSonicBetaAutonomous.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltraSonicBetaAutonomous.java
@@ -1,8 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 /**
  * Created by njmin on 2017-12-02.
  */
 
-public class UltraSonicBetaAutonomous {
+public class UltraSonicBetaAutonomous extends LinearOpMode {
+    
+
+
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //init code
+
+
+
+
+        waitForStart();
+        // wiat for play button pressed
+
+        while (opModeIsActive()){
+
+
+        }
+    }
 }
